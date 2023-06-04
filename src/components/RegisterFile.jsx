01d418c0dd56d85ce6d39e2dc5fac498884e0e9e
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-04 18:12:23 -0400

    file registration works

--- src/components/RegisterFile.jsx
+++ src/components/RegisterFile.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStore from '../store/fileRegistrationStore';
+import '../styles/RegisterFile.css'; 
 
 const RegisterFile = () => {
-  const { uploadFile, loading, message, summary, setSummary, dueDate, setDueDate, setFile } = useStore();
+  const { 
+    uploadFile, 
+    loading, 
+    message, 
+    summary, 
+    setSummary, 
+    dueDate, 
+    setDueDate, 
+    setFile, 
+    courts, 
+    getCourts, 
+    documentTypes, 
+    getDocumentTypes, 
+    selectedCourt, 
+    setSelectedCourt, 
+    selectedDocumentType, 
+    setSelectedDocumentType 
+  } = useStore();
+
+  useEffect(() => {
+    getCourts();
+    getDocumentTypes();
+  }, [getCourts, getDocumentTypes]);
 
   const onFileChange = (event) => {
     const file = event.target.files[0];
@@ -15,12 +38,20 @@ const RegisterFile = () => {
   }
 
   return (
-    <form onSubmit={onSubmit} >
+    <form onSubmit={onSubmit} className='register-file-form'>
+      <label>Corte:</label>
+      <select onChange={(e) => setSelectedCourt(parseInt(e.target.value))} disabled={loading} value={selectedCourt}>
+        {courts.map(court => <option key={court.idCourt} value={court.idCourt}>{court.courtName}</option>)}
+      </select>
+      <label>Tipo de documento:</label>
+      <select onChange={(e) => setSelectedDocumentType(parseInt(e.target.value))} disabled={loading} value={selectedDocumentType}>
+        {documentTypes.map(type => <option key={type.typeId} value={type.typeId}>{type.name}</option>)}
+      </select>
       <input type="file" onChange={onFileChange} disabled={loading} />
       <input type="text" value={summary} onChange={(e) => setSummary(e.target.value)} placeholder="Resumen" disabled={loading}  />
       <input type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} disabled={loading}  />
       <button type="submit" disabled={loading} >Enviar</button>
-      {loading ? <p >Cargando...</p> : <p>{message}</p>}
+      {loading ? <p>Cargando...</p> : <p>{message}</p>}
     </form>
   );
 };
--- src/store/fileRegistrationStore.js
+++ src/store/fileRegistrationStore.js
@@ -7,9 +7,23 @@ export const useStore = create((set, get) => ({
   summary: '',
   dueDate: '',
   file: null,
+  courts: [],
+  documentTypes: [],
+  selectedCourt: '',
+  selectedDocumentType: '',
   setSummary: (summary) => set({ summary }),
   setDueDate: (dueDate) => set({ dueDate }),
   setFile: (file) => set({ file }),
+  setSelectedCourt: (court) => set({ selectedCourt: court }),
+  setSelectedDocumentType: (type) => set({ selectedDocumentType: type }),
+  getCourts: async () => {
+    const response = await axios.get('http://localhost:8080/api/v1/Court');
+    set({ courts: response.data.response });
+  },
+  getDocumentTypes: async () => {
+    const response = await axios.get('http://localhost:8080/api/v1/LegalType');
+    set({ documentTypes: response.data.response });
+  },
   uploadFile: async () => {
     if (get().file === null) {
       set({ message: 'Por favor, selecciona un archivo.' });
@@ -17,9 +31,18 @@ export const useStore = create((set, get) => ({
     }
     set({ loading: true, message: '' });
     const formData = new FormData();
+    // aqui se pone el id del instance legal case
+
+    formData.append('instanceCaseId', 1);
     formData.append('file', get().file);
     formData.append('summary', get().summary);
     formData.append('dueDate', get().dueDate);
+    formData.append('courtId', get().selectedCourt ? get().selectedCourt : '1');
+    formData.append('documentTypeId', get().selectedDocumentType ? get().selectedDocumentType : '1');
+    //formData.append('courtId', get().selectedCourt); // Agregamos courtId a la FormData
+    //formData.append('documentTypeId', get().selectedDocumentType); // Agregamos documentTypeId a la FormData
+    console.log("a");
+    console.log(get().selectedCourt);
     try {
       const response = await axios.post('http://localhost:8080/upload', formData, {
         headers: {
