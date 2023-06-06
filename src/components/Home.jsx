badf27b508eec46e55e332960a0f82f16c188ce9
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-06 14:39:04 -0400

    Homejsx and home css changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,36 +31,37 @@ const Home = () => {
   
   return (
     <>
-    {status === 'loading' && <p>Cargando...</p>}
-    <Navbar></Navbar>
-    <div className="container">
-      <h1>Home</h1>
+      {status === 'loading' && <p>Cargando...</p>}
+      <Navbar></Navbar>
       
+      <div className="cases-container">
+        <h1>Casos</h1>
+        
 
-      {status === 'success' && cases.map((legalCase) => (
-        <div key={legalCase.idLegalCase} className="card">
-          <h2>{legalCase.title}</h2>
-          <p className="last-modified">Última modificación: {formatDate(legalCase.lastUpdate)}</p>
-          <p>{legalCase.summary}</p>
-          <p>{legalCase.crime}</p>
-          <button onClick={() => handleCaseClick(legalCase.idLegalCase)}>Ver Más</button>
-        </div>
-      ))}
+        {status === 'success' && cases.map((legalCase) => (
+          <div key={legalCase.idLegalCase} className="card">
+            <h2>{legalCase.title}</h2>
+            <p className="last-modified">Última modificación: {formatDate(legalCase.lastUpdate)}</p>
+            <p>{legalCase.summary}</p>
+            <p>{legalCase.crime}</p>
+            <button onClick={() => handleCaseClick(legalCase.idLegalCase)}>Ver Más</button>
+          </div>
+        ))}
 
-      {status === 'empty' && <p>No tienes casos registrados</p>}
-       {/* Controles de navegación */}
-       {status === 'success' && (
-        <div className="pagination">
-          <button onClick={previousPage} disabled={currentPage === 0}>
-            Anterior
-          </button>
-          <span>{currentPage + 1}</span>
-          <button onClick={nextPage} disabled={currentPage === totalPages - 1}>
-            Siguiente
-          </button>
-        </div>
-      )}
-    </div>
+        {status === 'empty' && <p>No tienes casos registrados</p>}
+        {/* Controles de navegación */}
+        {status === 'success' && (
+          <div className="pagination">
+            <button onClick={previousPage} disabled={currentPage === 0}>
+              Anterior
+            </button>
+            <span>{currentPage + 1}</span>
+            <button onClick={nextPage} disabled={currentPage === totalPages - 1}>
+              Siguiente
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
