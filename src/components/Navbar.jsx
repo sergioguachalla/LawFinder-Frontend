1c13cae245e5a993b6d123ffac04762807f78d46
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-06 14:10:24 -0400

    navbar styles changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,46 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import { useNavbarStore } from '../store/navbarStore';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBriefcase, faEnvelopeOpenText, faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
    const {username,setUsername,getUserNameFromToken} = useNavbarStore();
+   
    useEffect(() => {
-     
        setUsername();
    }, [setUsername, username]);
 
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/Home" className="navbar-link">Inicio</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/Home" className="navbar-link">Casos</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/Invitation" className="navbar-link">Invitaciones</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/" className="navbar-link">Salir</Link>
-        </li>
-        
-        <li>
-            {username}
-        </li>
-      </ul>
+      <div className="links-container">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/Home" className="navbar-link">
+              <FontAwesomeIcon icon={faHome} /> Inicio
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/Home" className="navbar-link">
+              <FontAwesomeIcon icon={faBriefcase} /> Casos
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/Invitation" className="navbar-link">
+              <FontAwesomeIcon icon={faEnvelopeOpenText} /> Invitaciones
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">
+              <FontAwesomeIcon icon={faSignOutAlt} /> Salir
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="username">
+        <FontAwesomeIcon icon={faUser} className="username-icon" /> 
+        <span className="username-text">{username}</span>
+      </div>
     </nav>
   );
 };
