16233093767146439530efde27023e36773f5777
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-20 12:04:27 -0400

    register case styles added

--- src/components/RegisterCase.jsx
+++ src/components/RegisterCase.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { useCaseStore } from '../store/caseRegistrationStore';
 import { useEffect } from 'react';
+import '../styles/RegisterCase.css';
+import Navbar from './Navbar';
 
 const RegisterCase = () => {
   const history = useNavigate();
@@ -65,101 +67,111 @@ const RegisterCase = () => {
 
   return (
     <>
-      <h1 className='centered-apple-font'>REGISTRO DE CASO</h1>
-      <form className="form-container" onSubmit={handleSubmitForm}>
-        <div className="form-row">
-          <label>Título *</label>
-          <input name="title" type="text" value={formData.title} onChange={(event) => handleChange('title', event)}  />
+    
+      <Navbar />
+      <h1 className='centered-apple-font-rc'>REGISTRO DE CASO</h1>
+    <div className="main-container-rc">
+      <form className="form-container-rc" onSubmit={handleSubmitForm}>
+        <div className="form-row-rc">
+          <div className="form-field-rc">
+            <label>Título *</label>
+            <input name="title" type="text" value={formData.title} onChange={(event) => handleChange('title', event)} />
+          </div>
+          <div className="form-field-rc">
+            <label>Fecha de inicio *</label>
+            <input name="startDate" type="date" value={formData.startDate} onChange={(event) => handleChange('startDate', event)} />
+          </div>
         </div>
-        <div className="form-row">
-          <label>Fecha de inicio *</label>
-          <input name="startDate" type="date" value={formData.startDate} onChange={(event) => handleChange('startDate', event)}  />
-        </div>
-        <div className="form-row">
+        <div className="form-row-inline-rc">
           <label>Resumen *</label>
-          <textarea name="summary" value={formData.summary} onChange={(event) => handleChange('summary', event)}  />
+          <textarea name="summary" value={formData.summary} onChange={(event) => handleChange('summary', event)} />
         </div>
-        <div className="form-row">
-          <label>Departamento*</label>
-          <select name="departamento" onChange={(event) => handleDepartamentoChange(event)} >
-            {departamentos.map((departamento) => (
-              <option key={departamento.idDepartment} value={departamento.idDepartment}>
-                {decodeURIComponent(departamento.departmentName)}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="form-row">
-          <label>Provincia*</label>
-          <select name="provincia" onChange={(event) => handleIsComplainant(event)} >
-            {provincias.map((provincia) => (
-              <option key={provincia.idProvince} value={provincia.idProvince}>
-                {decodeURIComponent(provincia.provinceName)}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="form-row">
-          <label>Categoría *</label>
-          <select name="categoria" onChange={(event) => handleCategoriaChange(event)}>
-            {categorias.map((categoria) => (
-                <option key={categoria.categoryId} value={categoria.categoryId}>
-                  {decodeURIComponent(categoria.categoryName)}
+        <div className="form-row-rc">
+          <div className="form-field-rc">
+            <label>Departamento*</label>
+            <select name="departamento" onChange={(event) => handleDepartamentoChange(event)} >
+              {departamentos.map((departamento) => (
+                <option key={departamento.idDepartment} value={departamento.idDepartment}>
+                  {decodeURIComponent(departamento.departmentName)}
                 </option>
-              ))
-            }
-          </select>
-        </div>
-        <div className="form-row">
-          <label>Sub-categoría *</label>
-          <select name="subCategoria" onChange={(event) => handleSubCategoriaChange(event)} >
-            {subCategorias.map((subCategoria) => (
-                <option key={subCategoria.idSubCategory} value={subCategoria.idSubCategory}>
-                  {decodeURIComponent(subCategoria.subCategoryName)}
+              ))}
+            </select>
+          </div>
+          <div className="form-field-rc">
+            <label>Provincia*</label>
+            <select name="provincia" onChange={(event) => handleIsComplainant(event)} >
+              {provincias.map((provincia) => (
+                <option key={provincia.idProvince} value={provincia.idProvince}>
+                  {decodeURIComponent(provincia.provinceName)}
                 </option>
-              ))
-            }
-          </select>
+              ))}
+            </select>
+          </div>
         </div>
-        {<div className="form-row">
-          <label>Delito *</label>
-          <select name="delito" onChange={(event) => handleChange('crimeId', event)} >
-            {crimes.map((crime) => (
-                <option key={crime.crimeId} value={crime.crimeId}>
-                  {decodeURIComponent(crime.name)}
-                </option>
-              ))
-            }
-          </select>
-        </div> }
-        <div className="form-row">
+        <div className="form-row-rc">
+          <div className="form-field-rc">
+            <label>Categoría *</label>
+            <select name="categoria" onChange={(event) => handleCategoriaChange(event)}>
+              {categorias.map((categoria) => (
+                  <option key={categoria.categoryId} value={categoria.categoryId}>
+                    {decodeURIComponent(categoria.categoryName)}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="form-field-rc">
+            <label>Sub-categoría *</label>
+            <select name="subCategoria" onChange={(event) => handleSubCategoriaChange(event)} >
+              {subCategorias.map((subCategoria) => (
+                  <option key={subCategoria.idSubCategory} value={subCategoria.idSubCategory}>
+                    {decodeURIComponent(subCategoria.subCategoryName)}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="form-field-rc">
+            <label>Delito *</label>
+            <select name="delito" onChange={(event) => handleChange('crimeId', event)} >
+              {crimes.map((crime) => (
+                  <option key={crime.crimeId} value={crime.crimeId}>
+                    {decodeURIComponent(crime.name)}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+        </div>
+        <div className="form-row-inline-rc">
           <label>Demandante (Si está marcado es demandante, caso contrario es demandado)</label>
-          <input name="complainant" type="checkbox" onChange={(event) => handleChange('complainant', event)}  />
+          <input name="complainant" type="checkbox" onChange={(event) => handleChange('complainant', event)} />
         </div>
         <hr />
         <h2>Actores</h2>
-        <div className="form-row">
-          <label>Invitar abogado *</label>
-          <input name="laywerEmail" type="text" onChange={(event) => handleLawyerEmail(event)}/>
-          <button type="button" onClick={() => handleInvitation("lawyer")}>Enviar invitación</button>
-          {status === 'lawyerNotFound' && <p>Abogado no registrado</p>}
-          {status === 'lawyerFound' && <p>Invitación enviada a {formData.lawyerEmail} </p>}
+        <div className="form-row-rc">
+          <div className="form-field-invitation-rc">
+            <label>Invitar abogado *</label>
+            <input name="laywerEmail" type="text" onChange={(event) => handleLawyerEmail(event)}/>
+            <button type="button" onClick={() => handleInvitation("lawyer")}>Enviar invitación</button>
+            {status === 'lawyerNotFound' && <p>Abogado no registrado</p>}
+            {status === 'lawyerFound' && <p>Invitación enviada a {formData.lawyerEmail} </p>}
+          </div>
+          <div className="form-field-invitation-rc">
+            <label>Invitar cliente *</label>
+            <input name="customerEmail" type="text" onChange={(event) => handleCustomerEmail(event)} />
+            <button type="button" onClick={() => {handleInvitation("customer")}}>Enviar invitación</button>
+            {status === 'customerNotFound' && <p>Cliente no registrado</p>}
+            {status === 'customerFound' && <p>Invitación enviada a {formData.lawyerEmail} </p>}
+          </div>
         </div>
-        <div className="form-row">
-          <label>Invitar cliente *</label>
-          <input name="customerEmail" type="text" onChange={(event) => handleCustomerEmail(event)} />
-          <button type="button" onClick={() => {handleInvitation("customer")}}>Enviar invitación</button>
-          {status === 'customerNotFound' && <p>Cliente no registrado</p>}
-          {status === 'customerFound' && <p>Invitación enviada a {formData.lawyerEmail} </p>}
-        </div>
-        
         <hr />
-        <div className="form-row">
+        <div className="form-row-inline-rc">
           <label>Contraparte *</label>
           <input name="counterpart" type="text" onChange={(event) => handleChange('counterpart', event)} />
         </div>
         <h2>Etapa actual del proceso</h2>
-        <div className="form-row">
+        <div className="form-row-inline-rc">
           <label>Instancia *</label>
           <select name="instancia" onChange={(event) => handleChange('idInstance', event)} >
             {instancias.map((instancia) => (
@@ -170,21 +182,23 @@ const RegisterCase = () => {
             }
           </select>
         </div>
-        <div className="form-row">
-          <label>Fecha de inicio de la instancia *</label>
-          <input name="fechaInicioInstancia" type="date" onChange={(event) => handleChange('startDateInstance', event)}  />
+        <div className="form-row-rc">
+          <div className="form-field-rc">
+            <label>Fecha de inicio de la instancia *</label>
+            <input name="fechaInicioInstancia" type="date" onChange={(event) => handleChange('startDateInstance', event)} />
+          </div>
+          <div className="form-field-rc">
+            <label>Fecha final de plazo de la instancia *</label>
+            <input name="fechaFinalInstancia" type="date" onChange={(event) => handleChange('endDateInstance', event)} />
+          </div>
         </div>
-        <div className="form-row">
-          <label>Fecha final de plazo de la instancia *</label>
-          <input name="fechaFinalInstancia" type="date" onChange={(event) => handleChange('endDateInstance', event)}  />
-        </div>
-        <div className="button-row">
-          <button className="register-button" onClick={()=> {navigate("/Home")}}>Cancelar</button>
-          <button type="submit" className="register-button">Registrar</button>
-          
+        <div className="button-row-rc">
+          <button className="register-button-rc" onClick={()=> {navigate("/Home")}}>Cancelar</button>
+          <button type="submit" className="register-button">Registrar</button>  
         </div>
       </form>
-    </>
+    </div>
+</>
   );
 };
 
