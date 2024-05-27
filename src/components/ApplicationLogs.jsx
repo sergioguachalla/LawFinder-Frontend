737c9600f61c5952d7521562640471b9e9437263
unknown <alejandro.flores.a@ucb.edu.bo>
2024-05-26 22:41:15 -0400

    logs added

--- src/components/ApplicationLogs.jsx
+++ src/components/ApplicationLogs.jsx
@@ -27,92 +27,92 @@ const ApplicationLogs = () => {
 
   return (
     <>
-        <Navbar />
-        <div>
-        <h1>Application Logs</h1>
+      <Navbar />
+      <div className="log-app-container">
+        <h1 className="log-app-title"> Logs De Aplicación</h1>
 
-        <div className="filter-container">
-            <div>
+        <div className="log-app-filter-container">
+          <div>
             <label htmlFor="levelId">Level: </label>
             <select id="levelId" value={levelId} onChange={(e) => setLevelId(e.target.value)}>
-                <option value="">All</option>
-                {levels.map(level => (
+              <option value="">All</option>
+              {levels.map(level => (
                 <option key={level.levelId} value={level.levelId}>{level.levelName}</option>
-                ))}
+              ))}
             </select>
 
             <label htmlFor="categoryId">Category: </label>
             <select id="categoryId" value={categoryId} onChange={(e) => setCategoryId(e.target.value)}>
-                <option value="">All</option>
-                {categories.map(category => (
+              <option value="">All</option>
+              {categories.map(category => (
                 <option key={category.categoryId} value={category.categoryId}>{category.categoryName}</option>
-                ))}
+              ))}
             </select>
 
             <label htmlFor="fromDate">From: </label>
             <input
-                type="date"
-                id="fromDate"
-                value={fromDate}
-                onChange={(e) => setFromDate(e.target.value)}
-                max={currentDate}
+              type="date"
+              id="fromDate"
+              value={fromDate}
+              onChange={(e) => setFromDate(e.target.value)}
+              max={currentDate}
             />
 
             <label htmlFor="toDate">To: </label>
             <input
-                type="date"
-                id="toDate"
-                value={toDate}
-                onChange={(e) => setToDate(e.target.value)}
-                min={fromDate}
-                max={currentDate}
+              type="date"
+              id="toDate"
+              value={toDate}
+              onChange={(e) => setToDate(e.target.value)}
+              min={fromDate}
+              max={currentDate}
             />
 
-            <button onClick={clearFilters}>Clear Filters</button>
-            </div>
+            <button className="log-app-clear-button" onClick={clearFilters}>Borrar Filtros</button>
+          </div>
         </div>
 
-        <div className="logs-container">
-            {status === 'loading' && <p>Loading...</p>}
-            {status === 'success' && (
+        <div className="log-app-logs-container">
+          {status === 'loading' && <p>Loading...</p>}
+          {status === 'success' && (
             <>
-                <table>
+              <table className="log-app-table">
                 <thead>
-                    <tr>
+                  <tr>
                     <th>ID</th>
+                    <th>Nivel</th>
                     <th>Username</th>
-                    <th>Date</th>
+                    <th>Fecha y hora</th>
                     <th>Host</th>
-                    <th>Description</th>
-                    <th>Category</th>
-                    <th>Level</th>
-                    </tr>
+                    <th>Evento</th>
+                    <th>Categoría</th>
+                  </tr>
                 </thead>
                 <tbody>
-                    {logs.map(log => (
+                  {logs.map(log => (
                     <tr key={log.logId}>
-                        <td>{log.logId}</td>
-                        <td>{log.username}</td>
-                        <td>{format(new Date(log.logDate), 'yyyy-MM-dd HH:mm:ss')}</td>
-                        <td>{log.host}</td>
-                        <td>{log.description}</td>
-                        <td>{log.category}</td>
-                        <td>{log.level}</td>
+                      <td>{log.logId}</td>
+                      <td>{log.level}</td>
+                      <td>{log.username}</td>
+                      <td>{format(new Date(log.logDate), 'dd-MM-yyyy HH:mm:ss')}</td>
+                      <td>{log.host}</td>
+                      <td>{log.description}</td>
+                      <td>{log.category}</td>
                     </tr>
-                    ))}
+                  ))}
                 </tbody>
-                </table>
-                <div className="pagination">
+              </table>
+              <div className="log-app-pagination">
                 <button onClick={previousPage} disabled={currentPage === 0}>Previous</button>
                 <span>{currentPage + 1}</span>
                 <button onClick={nextPage} disabled={currentPage === totalPages - 1}>Next</button>
-                </div>
+              </div>
             </>
-            )}
-            {status === 'empty' && <p>No logs found</p>}
-            {status === 'error' && <p>Error fetching logs</p>}
+          )}
+          {status === 'empty' && <p>No se encontraron Logs</p>}
+          {status === 'error' && <p>Error fetching logs</p>}
         </div>
-        </div>
+      </div>
     </>
   );
 };
--- src/store/securityLogsStore.js
+++ src/store/securityLogsStore.js
@@ -1,34 +1,31 @@
 import { create } from 'zustand';
 import axios from 'axios';
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const useLogsStore = create((set, get) => ({
   logs: [],
-  levels: [],
   categories: [],
   status: 'init',
   currentPage: 0,
   totalPages: 0,
   fromDate: '',
   toDate: '',
-  levelId: '',
   categoryId: '',
   
   setFromDate: date => set({ fromDate: date, currentPage: 0 }), 
   setToDate: date => set({ toDate: date, currentPage: 0 }), 
-  setLevelId: id => set({ levelId: id, currentPage: 0 }), 
   setCategoryId: id => set({ categoryId: id, currentPage: 0 }), 
   
   getLogs: async () => {
     try {
       set({ status: 'loading' });
-      const response = await axios.get(`${API_URL}/applicationLogs`, {
+      const response = await axios.get(`${API_URL}/securityLogs`, {
         params: {
           page: get().currentPage,
           size: 10, // Tamaño de página
           from: get().fromDate,
           to: get().toDate,
-          levelId: get().levelId,
           categoryId: get().categoryId,
         }
       });
@@ -47,17 +44,6 @@ export const useLogsStore = create((set, get) => ({
     } catch (error) {
       console.error('Error fetching logs:', error);
       set({ status: 'error' });
-    }
-  },
-
-  getLevels: async () => {
-    try {
-      const response = await axios.get(`${API_URL}/logsLevel`);
-      if (response.data.code === '0000') {
-        set({ levels: response.data.response });
-      }
-    } catch (error) {
-      console.error('Error fetching levels', error);
     }
   },
 
@@ -90,7 +76,6 @@ export const useLogsStore = create((set, get) => ({
     set({
       fromDate: '',
       toDate: '',
-      levelId: '',
       categoryId: '',
       currentPage: 0,
     });
