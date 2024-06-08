212be8bc426bf88c17f6d9b9b6df6b1b6f053f3a
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-06-07 22:33:33 -0400

    add login automated test

---
+++ src/tests/login.test.js
@@ -0,0 +1,16 @@
+import { Selector } from 'testcafe';
+
+fixture `Login Test`
+  .page `http://localhost:5174`;
+
+test('Login with invalid credentials', async t => {
+  const errorMessage = Selector('.error-message');
+  const loginButton = Selector('.login-button');
+
+  await t
+    .typeText('#username', 'admin')
+    .typeText('#password', 'admin')
+    .click(loginButton)
+    .expect(errorMessage.innerText).eql('Usuario o contraseña incorrectos');
+});
+
