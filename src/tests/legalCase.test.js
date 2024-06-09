a0ba7dc8e435c4c54a395de4d206416c62ae1876
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-06-09 17:37:08 -0400

    update test

---
+++ src/tests/legalCase.test.js
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe';
+
+fixture `Login Test`
+  .page `http://localhost:5173`;
+
+test('Login with invalid credentials: -admin -admin', async t => {
+  const errorMessage = Selector('#error-message');
+  const loginButton =  Selector('#login-button');
+
+  await t
+    .typeText('#username', 'admin')
+    .typeText('#password', 'admin')
+    .click('#login-button')
+    .wait(4000)
+    .expect(errorMessage.innerText).eql('Usuario o contraseña incorrectos');
+
+});
